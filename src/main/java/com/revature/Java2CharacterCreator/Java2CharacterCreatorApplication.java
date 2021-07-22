b9616aa820780be7bdc5f7f30df6f8a61aa4093c
RMaxwell404 <maxwellryan404@gmail.com>
2021-07-22 10:48:32 -0400

    first commit

---
+++ src/main/java/com/revature/Java2CharacterCreator/Java2CharacterCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Java2CharacterCreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2CharacterCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java2CharacterCreatorApplication.class, args);
+	}
+
+}
