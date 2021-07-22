8aead960df84fcdec9d1174150dd050522d69a19
RMaxwell404 <maxwellryan404@gmail.com>
2021-07-22 13:06:30 -0400

    Refactored character to fit java naming schemes, added description generator test

--- src/test/java/com/revature/service/DescriptionMakerTest.java
+++ src/test/java/com/revature/service/DescriptionMakerTest.java
@@ -1,4 +1,17 @@
+package com.revature.service;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DescriptionMakerTest {
-  
+
+    private DescriptionMaker dMaker = new DescriptionMaker();
+
+    @Test
+    public void makeDescriptionTest(){
+        System.out.println(dMaker.makeDescription());
+
+    }
+
 }
