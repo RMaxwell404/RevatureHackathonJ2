b00016eea496eaab6d220b69c2cc310a980a93bd
So0123 <so.morishima@gmail.com>
2021-07-22 18:40:55 -0500

    deleted unneeded files

--- src/main/java/com/revature/exceptions/CharacterException.java
+++
@@ -1,9 +0,0 @@
-package com.revature.exceptions;
-
-public class CharacterException extends Exception{
-    private static final long serialVersionUID = 1L;
-
-    public CharacterException(final String message) {
-        super(message);
-    }
-}
